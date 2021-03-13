7c698170010e67bb2ec989543921f9113c94a959
Frankie Mercado <oeluer@gmail.com>
2021-03-13 02:51:08 -0800

    added splits

--- main.js
+++ main.js
@@ -1,6 +1,285 @@
 let nextTarget = 'f8';
 let showTargets = true;
 let timerInterval;
+let moveCount = 0;
+let targetCount = 0;
+
+let statsDisplay = document.getElementById('statsDisplay');
+
+function updateStatsDisplay(stats) {
+  let rows = [...statsDisplay.children];
+  for (let i = 0; i < stats.length; i++) {
+    rows[i].children[0].textContent = stats[i].square;
+    rows[i].children[1].textContent = stats[i].optimal;
+    rows[i].children[2].textContent = stats[i].moves;
+    rows[i].children[3].textContent = stats[i].split;
+  }
+
+  // for (let obj of stats) {
+  //   let tr = document.createElement('tr');
+  //   tr.innerHTML = `<th>${obj.square}</th>
+  //   <td>${obj.optimal}</td>
+  //   <td>${obj.moves}</td>
+  //   <td>${obj.split}</td>`;
+  //   statsDisplay.appendChild(tr);
+  // }
+}
+
+let stats = [
+  {
+    square: 'h8',
+    moves: 0,
+    optimal: 0,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'f8',
+    moves: 0,
+    optimal: 2,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'd8',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'c8',
+    moves: 0,
+    optimal: 6,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'b8',
+    moves: 0,
+    optimal: 9,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'h7',
+    moves: 0,
+    optimal: 5,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'g7',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'e7',
+    moves: 0,
+    optimal: 6,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'c7',
+    moves: 0,
+    optimal: 6,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'a7',
+    moves: 0,
+    optimal: 8,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'h6',
+    moves: 0,
+    optimal: 8,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'g6',
+    moves: 0,
+    optimal: 5,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'f6',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'b6',
+    moves: 0,
+    optimal: 6,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'a6',
+    moves: 0,
+    optimal: 7,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'h4',
+    moves: 0,
+    optimal: 7,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'g4',
+    moves: 0,
+    optimal: 5,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'f4',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'b4',
+    moves: 0,
+    optimal: 6,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'a4',
+    moves: 0,
+    optimal: 7,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'h3',
+    moves: 0,
+    optimal: 4,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'g3',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'e3',
+    moves: 0,
+    optimal: 2,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'c3',
+    moves: 0,
+    optimal: 4,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'a3',
+    moves: 0,
+    optimal: 2,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'h2',
+    moves: 0,
+    optimal: 4,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'f2',
+    moves: 0,
+    optimal: 2,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'e2',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'c2',
+    moves: 0,
+    optimal: 4,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'b2',
+    moves: 0,
+    optimal: 5,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'g1',
+    moves: 0,
+    optimal: 4,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'f1',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'e1',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'c1',
+    moves: 0,
+    optimal: 6,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'b1',
+    moves: 0,
+    optimal: 3,
+    time: 0,
+    split: 0,
+  },
+  {
+    square: 'a1',
+    time: 0,
+    split: 0,
+    moves: 0,
+    optimal: 3,
+  }
+];
+
 
 function inQVision(square) {
   // Queen on d5;
@@ -31,7 +310,6 @@ function getNextTarget(prev) {
       let duration = new Date(ms).toISOString().substr(11, 8);
       document.getElementById('timerDisplay').classList.remove('is-hidden');
       clearInterval(timerInterval);
-      t_start = performance.now();
     }
   }
 
@@ -53,9 +331,10 @@ function highlightSquare(square) {
   }
 }
 
-let moveCount = document.getElementById('moveCount');
-let targetCount = document.getElementById('targetsHit');
-let t_start = performance.now();
+let movesDisplay = document.getElementById('moveCount');
+let targetCountDisplay = document.getElementById('targetsHit');
+let t_start;
+let t_checkpoint;
 
 let config = {
   draggable: true,
@@ -72,32 +351,39 @@ let config = {
     if (inQVision(target) || !legalKnight(source, target)) {
       return "snapback";
     }
+    movesDisplay.textContent = ++moveCount;
+    stats[targetCount + 1].moves++;
     if (target === nextTarget) {
-      targetCount.textContent = parseInt(targetCount.textContent) + 1;
+      targetCountDisplay.textContent = ++targetCount;
+      stats[targetCount].time = performance.now();
+      stats[targetCount].split = ((stats[targetCount].time - stats[targetCount - 1].time) / 1000).toFixed(2);
+      updateStatsDisplay(stats);
       nextTarget = getNextTarget(nextTarget);
       clearHighlights();
       showQV();
       highlightSquare(nextTarget);
     }
   },
-  onChange: () => {
-    moveCount.textContent = parseInt(moveCount.textContent) + 1;
-  },
   onDragMove: () => {
-    if (moveCount.textContent.trim() === '0' && !timerInterval) {
+    if (moveCount === 0 && !timerInterval) {
       t_start = performance.now();
+      stats[0].time = t_start;
       timerInterval = setInterval(updateTimer, 1000);
     }
   },
 };
 var board = Chessboard("board", config);
 highlightSquare(nextTarget);
+updateStatsDisplay(stats);
 
 document.getElementById('reset').addEventListener('click', () => {
   board.position('7N/8/8/3q4/8/8/8/8 w - - 0 1');
   nextTarget = 'f8';
-  moveCount.textContent = '0';
-  targetCount.textContent = '0';
+  moveCount = 0;
+  stats.forEach(o => { o.moves = 0; o.time = 0; o.split = 0; })
+  movesDisplay.textContent = moveCount;
+  targetCount = 0;
+  targetCountDisplay.textContent = targetCount;
   clearHighlights();
   showQV();
   highlightSquare(nextTarget);
@@ -105,6 +391,7 @@ document.getElementById('reset').addEventListener('click', () => {
   clearInterval(timerInterval);
   timerInterval = null;
   updateTimer();
+  updateStatsDisplay(stats);
 });
 
 function updateTimer() {
@@ -150,7 +437,11 @@ function showQV() {
 document.getElementById('queenVision').addEventListener('click', () => {
   qv = !qv;
   showQV();
-})
+});
+
+document.getElementById('statsButton').addEventListener('click', () => {
+  statsDisplay.parentElement.classList.toggle('is-hidden');
+});
 
 $(window).resize(() => {
   board.resize();
